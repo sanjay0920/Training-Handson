7b8f3cacbfa9cf043cadccab39229a16ae8c7c05
sanjay0920 <sanjaybharathis18087@it.ssn.edu.in>
2022-08-23 16:26:47 +0530

    exercises

---
+++ 23-08-2022/23-08-2022/ps2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace application
+{
+    class program
+    {
+        public static int addition(int a, int b)
+        {
+            return (a + b);
+        }
+
+        public static void Main()
+        {
+            int a, b;
+            int sum;
+
+            Console.Write("Enter first number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            sum = addition(a, b);
+
+            Console.WriteLine("Sum is: " + sum);
+            Console.ReadLine();
+        }
+    }
+}
