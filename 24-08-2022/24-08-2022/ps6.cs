66c5f5cc46a9ebf3af97b62f420062b3a5db317c
sanjay0920 <sanjaybharathis18087@it.ssn.edu.in>
2022-08-24 20:39:37 +0530

    exercises

---
+++ 24-08-2022/24-08-2022/ps6.cs
@@ -0,0 +1,22 @@
+﻿namespace Logicofapplication
+{
+    public class Ps6
+    {
+        public static void Greet()
+        {
+            Console.WriteLine("hello");
+
+        }
+        public static void Makefile()
+        {
+
+            string path = @" C:\Users\SANJAY STARK\source\repos\sanjay-git\24-08-2022\sanjay.txt";
+            File.Create(path);
+
+
+
+        }
+
+
+    }
+}
