833c11d3b016801863baf2ffc320090c5dd7adc0
sanjay0920 <sanjaybharathis18087@it.ssn.edu.in>
2022-09-02 15:24:08 +0530

    exercises

---
+++ 02-09-2022 OOPS/02-09-2022 OOPS/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using categorylibrary;
+using productlibrary;
+
+namespace demo
+{
+    class program
+    {
+
+
+        static void Main(string[] args)
+        {
+            category cat = new category();
+            cat.CATID = 01;
+            cat.CATNAME = "dairy";
+            cat.DESCRIPTION = "p";
+
+            product prod = new product();
+            prod.PRODUCTID = 001;
+            prod.PRODUCTNAME = "milk";
+            prod.QUANTITYSOLD = 20;
+            prod.UNITPRICE = 25;
+            prod.UNITOFMEASUREMENT = "litre";
+            prod.QUANTITYINHAND = 5;
+
+            Console.WriteLine(cat.DESCRIPTION);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 02-09-2022 OOPS/productclass/product.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace productlibrary
+{
+    public class product
+    {
+        int _productID;
+
+        public int PRODUCTID
+        {
+            get { return _productID; }
+            set { _productID = value; }
+        }
+
+        string _productName;
+
+        public string PRODUCTNAME
+        {
+            get { return _productName; }
+            set { _productName = value; }
+        }
+
+        int _qntysold;
+        public int QUANTITYSOLD
+        {
+            get { return _qntysold; }
+            set { _qntysold = value; }
+        }
+
+        int _unitprice;
+        public int UNITPRICE
+        {
+            get { return _unitprice; }
+            set { _unitprice = value; }
+        }
+
+        string _uomes;
+        public string UNITOFMEASUREMENT
+        {
+            get { return _uomes; }
+            set { _uomes = value; }
+        }
+
+        int _qihand;
+        public int QUANTITYINHAND
+        {
+            get { return _qihand; }
+            set { _qihand = value; }
+        }
+
+
+    }
+}
