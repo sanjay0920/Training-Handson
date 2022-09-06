90e53896e46d1348851579b88004b6c217925222
sanjay0920 <sanjaybharathis18087@it.ssn.edu.in>
2022-09-06 11:13:11 +0530

    exercises

---
+++ 05-09-2022 OOPS/class/Class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ClassLibrary1
+{
+    public class Book
+    {
+        private int Bookid;
+        public int ID
+        {
+            get { return Bookid; }
+            set { Bookid = value; }
+        }
+        private string Bookname;
+        public string Name
+        {
+            get { return Bookname; }
+            set { Bookname = value; }
+        }
+        private int Price;
+        public int price
+        {
+            get { return Price; }
+            set { price = value; }
+
+        }
+        private string Author;
+        public string name
+        {
+            get { return Author; }
+            set { Author = value; }
+        }
+        private int categoryid;
+        public int iD
+
+        {
+            get { return categoryid; }
+            set { categoryid = value; }
+        }
+        private string categoryname;
+        public string NAME
+        {
+            get { return categoryname; }
+            set { categoryname = value; }
+        }
+        private string description;
+        public string Desc
+        {
+            get { return description; }
+            set { description = value; }
+
+        }
+        public void InsertData(int Bookid, string Bookname, int Price)
+        {
+            this.ID = Bookid;
+            this.Name = Bookname;
+            this.price = Price;
+
+        }
+        class Novel : Book
+        {
+            public new void InsertData(int Bookid, string Bookname, int Price)
+            {
+                this.ID = Bookid;
+                this.Name = Bookname;
+                this.price = Price;
+
+            }
+        }
+    }
+}
