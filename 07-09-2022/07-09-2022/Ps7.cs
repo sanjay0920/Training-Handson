9c930b08fac3b50937e0043690fc977af419a57c
sanjay0920 <sanjaybharathis18087@it.ssn.edu.in>
2022-09-07 17:14:39 +0530

    exercises

---
+++ 07-09-2022/07-09-2022/Ps7.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+namespace exercise
+{
+    delegate string delegate_method(string str);
+    class delegate_test
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "sanjay bharathi";
+            Console.WriteLine("string: " + str1);
+            Console.WriteLine("converted string:\n" + test(str1));
+           
+        }
+        public static string test(string str1)
+        {
+            return string.Join(" ", str1.Split(' ').Select(str1 => char.ToUpper(str1[0]) + str1.Substring(1)));
+        }
+    }
+}
