211f4d9a65ee51a3c62509aa856160c65201cc59
sanjay0920 <sanjaybharathis18087@it.ssn.edu.in>
2022-09-09 13:30:08 +0530

    exercises

--- 08-09-2022/08-09-2022/Employeeoperations.cs
+++ 08-09-2022/08-09-2022/Employeeoperations.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace _08_09_2022
 {
     public delegate void handler(Employee emp);
@@ -56,16 +57,16 @@ namespace _08_09_2022
             display del = new display(display);
             del();
             Console.ReadLine();
-        }     
-                    static void display()
-        { 
-                    foreach (var item in employees)
+        }
+        static void display()
+        {
+            foreach (var item in employees)
             {
                 Console.WriteLine(item.empname);
                 Console.WriteLine(item.deptno);
             }
 
-            }
+        }
         static void insert(Employee emp)
         {
             Employee employee = new Employee();
@@ -116,9 +117,30 @@ namespace _08_09_2022
             employees.Add(employee);
             Console.WriteLine("employee updated.");
         }
-		
+
+        public void Displaybymgr(List<Employee> m)
+        {
+            Console.WriteLine("Enter the name of the manager whom name of employees you want to see ");
+            string s = Console.ReadLine();
+            var ti = m.Where(mi => mi.manager == s).ToList();
+            if (ti.Count > 0)
+            {
+                foreach (var item in ti)
+                    Console.WriteLine(item.empname);
+            }
+            else
+            {
+                Console.WriteLine("No employees is working under him");
+            }
+        }
+        public void Display(List<Employee> m)
+        {
+            foreach (var item in m)
+            {
+                Console.WriteLine($"{item.empid} {item.empname} {item.deptno} {item.salary} {item.manager}\n");
+            }
 
 
-
-	}
+        }
+    }
 }
