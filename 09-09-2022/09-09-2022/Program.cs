21dc9ed01c27845b35dd3445aa3f55ac3d10bfcb
sanjay0920 <sanjaybharathis18087@it.ssn.edu.in>
2022-09-10 21:41:05 +0530

    exercises

---
+++ 09-09-2022/09-09-2022/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace application
+{
+    class Program
+    {
+        public static void M1()
+        {
+            Console.WriteLine("function called using Thread");
+        
+        }
+         public static void M2(object data)
+        {
+            Console.WriteLine("function called using parameterizedthreadstart Delegate :" + data);
+        }
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(M1);
+            Thread t2 = new Thread(M2);
+        }
+    }
+}
+   
---
+++ 09-09-2022/ClassLibrary1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+namespace ClassLibrary1
+{
+    public class Threading
+    {
+        static void M1()
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine("sleep state =" + Thread.CurrentThread.ThreadState);
+        }
+    }
+}
