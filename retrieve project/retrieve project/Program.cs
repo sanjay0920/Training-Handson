4c59559baa756f74291efd622dd5d225353b3fda
sanjay0920 <sanjaybharathis18087@it.ssn.edu.in>
2022-09-16 12:18:15 +0530

    project

---
+++ retrieve project/retrieve project/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace retrieve_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText(@"C:\datas\studentsdata.txt");
+            Console.WriteLine(text);
+        }
+    }
+}
